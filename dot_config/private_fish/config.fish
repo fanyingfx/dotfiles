886f11bb96435de28ff2275b93c7b36f0ef6232e
fanyingfx <fanyingfx@outlook.com>
2024-10-01 08:38:17 +0800

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -15,6 +15,7 @@ set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 
 set -x ELECTRON_OZONE_PLATFORM_HINT auto
 fish_add_path ~/.config/emacs/bin
+fish_add_path ~/scripts
 #fish_default_key_bindings
 #fish_vi_key_bindings
 #bind -M insert \cp history-search-backward
