f739f6ded84c019d9dac9221afed6ef5534a5391
fanyingfx <fanyingfx@outlook.com>
2024-10-03 16:29:30 +0800

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -39,3 +39,11 @@ function virc
     source $config_path
 end
 
+function yy
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
