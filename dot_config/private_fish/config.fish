e96b93b4cdb015a4cf8ef4627abe700ed463219c
fanyingfx <fanyingfx@outlook.com>
2025-05-24 21:44:42 +0800

    Update .config/kanata/config.kbd Update .config/mpv/profiles.conf Update .config/mpv/saved-props.json Update .config/fish/config.fish Update .zshrc

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,13 +1,13 @@
 if status is-interactive
     starship init fish | source
-    alias ls  'eza -l'
+    alias ls 'eza -l'
     alias vim nvim
     alias edit 'chezmoi edit --apply'
-    alias open 'xdg-open'
+    alias open xdg-open
     alias restart-plasma 'killall plasmashell && kstart plasmashell'
-    alias cl 'clear'
+    alias cl clear
     alias clt '~/myscripts/clean_text.py'
-    alias gd 'goldendict'
+    alias gd goldendict
     alias mount_smb 'sudo ~/myscripts/mount_smb.sh'
     alias rg 'rg -uu --glob \'!.git\''
     alias grep rg
@@ -16,11 +16,10 @@ if status is-interactive
     alias g++w 'g++ -Wall -Wextra'
     alias clang++ 'clang++ -Wall -Wextra'
     alias clang 'clang -Wall -Wextra'
-    alias rm "echo Use 'del', or the full path i.e. '/bin/rm'"   
+    alias rm "echo Use 'del', or the full path i.e. '/bin/rm'"
     alias gccw 'gcc -Wall -Wextra'
     alias disasm 'objdump -drwC -Mintel'
     alias gcp 'git clone (wl-paste)'
-    alias hx helix
     atuin init fish | source
     zoxide init fish | source
 
@@ -39,20 +38,18 @@ set local_proxy 'http://127.0.0.1:7890'
 
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -x GOPATH $HOME/.go
-set -x VISUAL /usr/bin/nvim
-set -x EDITOR /usr/bin/nvim
+set -x VISUAL /usr/bin/helix
+set -x EDITOR /usr/bin/helix
 set -x SYSTEMD_EDITOR /usr/bin/nvim
 set -x ELECTRON_OZONE_PLATFORM_HINT auto
 set -x DLPFILE "%(title)s.%(ext)s"
 set -x DLPFOLDER $HOME/Videos/ytb
 set -x OCAMLRUNPARAM b
-set -x VCPKG_ROOT  /home/fan/code/cpp/vcpkg/
+set -x VCPKG_ROOT /home/fan/code/cpp/vcpkg/
 set -x ZVM_INSTALL $HOME/.zvm/self
 set -x https_proxy $local_proxy
-set -x http_proxy $local_proxy 
+set -x http_proxy $local_proxy
 set -x all_proxy $local_proxy
-
-
 
 # set for nju pa
 set -x NEMU_HOME /home/fan/code/c/ics2024/nemu
@@ -66,7 +63,7 @@ fish_add_path $ZVM_INSTALL
 fish_add_path $HOME/.zvm/bin
 fish_add_path $HOME/bin
 fish_add_path $HOME/.moon/bin
-fish_add_path $HOME/.local/bin 
+fish_add_path $HOME/.local/bin
 
 #export MANWIDTH=999
 # https://code.visualstudio.com/docs/terminal/shell-integration
@@ -74,7 +71,7 @@ fish_add_path $HOME/.local/bin
 function virc
     set config_path $HOME/.config/fish/config.fish
     #chezmoi edit --apply $config_path
-    vim $config_path
+    helix $config_path
     source $config_path
 end
 function yt-down
@@ -97,21 +94,21 @@ function play-text
 end
 
 function yy
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	/bin/rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    /bin/rm -f -- "$tmp"
 end
 function tree
     set argc (count $argv)
     if test $argc -eq 0
-	eza -T --level 1
+        eza -T --level 1
     else if test $argc -eq 1
-	eza -T  $argv[1] --level 1
-    else 
-	eza -T  $argv[1] --level $argv[2]
+        eza -T $argv[1] --level 1
+    else
+        eza -T $argv[1] --level $argv[2]
     end
 end
 function cdf
@@ -142,10 +139,10 @@ function chcd
 end
 
 function git
-    if test "$argv[1]" = "push" && test "$argv[2]" = "-f"
+    if test "$argv[1]" = push && test "$argv[2]" = -f
         echo '⚠️ trying to force push... [git push -f]'
         read -l -P 'continue? [y/N]: ' confirm
-        if test "$confirm" != "y"
+        if test "$confirm" != y
             echo "❌ canceled force push"
             return 1
         end
@@ -158,10 +155,9 @@ function codep
     set rest $argv[2..-1]
     command code --profile $profile $rest
 end
-set -l profiles "zig moonbit haskell ocaml rust python odin c" 
-complete -c codep -n "not __fish_seen_subcommand_from $profiles" -f -a "$profiles" 
+set -l profiles "zig moonbit haskell ocaml rust python odin c"
+complete -c codep -n "not __fish_seen_subcommand_from $profiles" -f -a "$profiles"
 
-test -r '/home/fan/.opam/opam-init/init.fish' && source '/home/fan/.opam/opam-init/init.fish' > /dev/null 2> /dev/null; or true
+test -r '/home/fan/.opam/opam-init/init.fish' && source '/home/fan/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
 # END opam configuration
 source $HOME/myscripts/predefined/conf.fish
-
