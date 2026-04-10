c0b9ae3c7545cd96d7d1f17b4eb101e8b4ba410e
fanyingfx <fanyingfx@outlook.com>
2026-04-10 17:05:57 +0800

    Update .config/mpv/saved-props.json Update .config/Code/User/keybindings.json Update .config/Code/User/settings.json Update .config/fish/config.fish Update .local/share/applications/anki.desktop

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,34 +1,18 @@
 if status is-interactive
+
     starship init fish | source
     atuin init fish --disable-up-arrow | source
     zoxide init fish | source
     jj util completion fish | source
+
     alias ls 'eza -snew'
     alias vim nvim
-    alias sudoedit 'doas helix'
-    # alias edit 'chezmoi edit --apply'
     alias open xdg-open
-    alias restart-plasma 'killall plasmashell && kstart plasmashell'
-    alias cl clear
-    alias clt '~/myscripts/clean_text.py'
-    alias gd goldendict
-    # alias mount_smb 'sudo ~/myscripts/mount_smb.sh'
-    alias prx proxychains
-    # alias rg 'rg -uu --glob \'!.git\''
-    alias grep rg
-    # alias codex 'code --ozone-platform=x11 --enable-ozone'
-    alias anki_export 'uv --directory /home/fan/code/python/sentence-flashcard run export.py'
-    alias g++w 'g++ -Wall -Wextra'
-    alias clang++ 'clang++ -Wall -Wextra'
-    alias clang 'clang -Wall -Wextra'
     alias del /bin/rm
     alias rm "echo Use 'del', or the full path i.e. '/bin/rm'"
-    alias gccw 'gcc -Wall -Wextra'
-    alias disasm 'objdump -drwC -Mintel'
-    abbr gcp 'git clone (wl-paste)'
     abbr rm_lock 'sudo rm /var/lib/pacman/db.lck'
     alias zig_watch 'zig build -p -Dno-lib --watch -fincremental --prominent-compile-errors'
-    alias rreset='echo -e "\033[0m"'
+    alias pirun='pi --model zai/glm-4.7 -p'
 
     bind \cf forward-word
     bind \cb backward-word
@@ -56,11 +40,7 @@ set -x DLPFOLDER $HOME/Videos/ytb
 set -x OCAMLRUNPARAM b
 set -x VCPKG_ROOT /home/fan/code/cpp/vcpkg/
 set -x ZVM_INSTALL $HOME/.zvm/self
-#set -x all_proxy $local_proxy
-
-# set for nju pa
-set -x NEMU_HOME /home/fan/code/c/ics2024/nemu
-set -x AM_HOME /home/fan/code/c/ics2024/abstract-machine
+set -x ERL_AFLAGS "-kernel shell_history enabled"
 
 fish_add_path ~/.config/emacs/bin
 fish_add_path ~/.ghcup/bin
@@ -73,10 +53,6 @@ fish_add_path $HOME/.local/bin
 fish_add_path $HOME/bin
 fish_add_path $HOME/.dotnet
 fish_add_path $HOME/.dotnet/tools
-
-#export MANWIDTH=999
-# https://code.visualstudio.com/docs/terminal/shell-integration
-#
 
 alias edit $EDITOR
 
@@ -103,7 +79,7 @@ function kw
     kd (wl-paste)
 end
 function play-text
-    edge-playback --text (wl-paste)
+    edge-playback --text
 end
 
 function yy
@@ -141,10 +117,6 @@ function crun
 
     gcc -o /tmp/a.out $file && /tmp/a.out
 end
-function viconf
-    /usr/bin/nvim $HOME/myscripts/predefined/conf.fish
-    source $HOME/myscripts/predefined/conf.fish
-end
 
 function chcd
     chezmoi re-add
@@ -173,31 +145,10 @@ end
 function ytb-mpv
     eza -I "*.png|*.srt" --absolute --sort=created --reverse /home/fan/Videos/ytb/ | head -n 1 | xargs mpv
 end
-function whisperx
-
-    uvx -p 3.12 whisperx --model small --compute_type int8 --output_format srt --chunk_size 15 $argv[1]
-
-end
 function fish_remove_path
     if set -l index (contains -i "$argv" $fish_user_paths)
         set -e fish_user_paths[$index]
         echo "Removed $argv from the path"
-    end
-end
-function toggle_proxy
-    if set -q all_proxy
-        set -e all_proxy
-        set -e https_proxy
-        set -e http_proxy
-        echo "Proxy disabled"
-
-    else
-
-        set -gx all_proxy $local_proxy
-        set -gx https_proxy $local_proxy
-        set -gx http_proxy $local_proxy
-        echo "Proxy enabled: all_proxy"
-
     end
 end
 function video_info
@@ -228,4 +179,12 @@ complete -c codep -n "not __fish_seen_subcommand_from $profiles" -f -a "$profile
 
 test -r '/home/fan/.opam/opam-init/init.fish' && source '/home/fan/.opam/opam-init/init.fish' >/dev/null 2>/dev/null; or true
 # END opam configuration
-source $HOME/myscripts/predefined/conf.fish
+#source $HOME/myscripts/predefined/conf.fish
+source $HOME/.api_key.fish
+
+# pnpm
+set -gx PNPM_HOME "/home/fan/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
